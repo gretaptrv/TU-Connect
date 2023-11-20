e0cd3864ac56f677b137c49760caa78d61a58091
greta.petrova <greta.petrova@paysafe.com>
2023-11-20 16:56:32 +0200

    Removed keycloak

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,34 +1,30 @@
 package com.example.demo;
 
-import com.example.demo.config.KeycloakServerProperties;
+import com.example.demo.services.EmailService;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class Demo1Application {
   private static final Logger LOG = LoggerFactory.getLogger(Demo1Application.class);
+  @Autowired
+  private EmailService emailService;
 
   public static void main(String[] args) {
     SpringApplication.run(Demo1Application.class, args);
   }
 
-  @Bean
-  ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(
-      ServerProperties serverProperties, KeycloakServerProperties keycloakServerProperties) {
-    return (evt) -> {
-      Integer port = serverProperties.getPort();
-      String keycloakContextPath = keycloakServerProperties.getContextPath();
-      LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak",
-               port, keycloakContextPath);
-    };
+  @PostConstruct
+  public void sendEmailOnStartup() {
+    emailService
+        .sendEmail("gweaverace@gmail.com", "diplomna rabota",
+                   "mnogo qk mail");
   }
 
 }
